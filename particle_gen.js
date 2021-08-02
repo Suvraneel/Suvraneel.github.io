fd59cbee4d4a7ca2b9df26c7816bbe9d06570154
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2021-08-02 15:35:53 +0530

    Update particle_gen.js

--- particle_gen.js
+++ particle_gen.js
@@ -3,10 +3,10 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 380,
+      "value": 250,
       "density": {
         "enable": true,
-        "value_area": 800
+        "value_area": 1000
       }
     },
     "color": {
