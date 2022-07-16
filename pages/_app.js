b47231e58b2c78a2f365511a09ac91248a75ec35
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-07-16 20:53:51 +0530

    spinkit loader implemented :ant:

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import "@fontsource/raleway/400.css";
 import "@fontsource/playfair-display"
-import NextNProgress from "nextjs-progressbar";
+// import NextNProgress from "nextjs-progressbar";
+import NextNProgress from "../components/NextNProgress";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Script from 'next/script'
+import Script from "next/script";
 import Head from "next/head";
 import { Calendly } from "../components/Calendly";
 import SplineObj from "../components/SplineObj";
@@ -8,26 +8,45 @@ const Contact = () => {
   useEffect(() => {
     // Calendly();
     const CalendlyScript = document.createElement("script");
-    CalendlyScript.src = "https://assets.calendly.com/assets/external/widget.js";
+    CalendlyScript.src =
+      "https://assets.calendly.com/assets/external/widget.js";
     document.body.appendChild(CalendlyScript);
   }, []);
   return (
-    <div className="flex flex-row justify-center relative left-10 h-screen">
-    <Head>
+    <div className="flex flex-row justify-center relative left-10 h-screen w-screen">
+      <Head>
         <title>Portfolio</title>
-    </Head>
-        {/* <Script
+      </Head>
+      {/* <Script
         type="text/javascript"
         src="https://assets.calendly.com/assets/external/widget.js"
         strategy="lazyOnload"
         async
       ></Script> */}
       <div className="h-full w-11/12 z-0">
-          <SplineObj scene={'./spline/sceneMUSHROOM.splinecode'}/>
+        <SplineObj scene={"./spline/sceneMUSHROOM.splinecode"} />
       </div>
-      <div className="w-2/3 h-full form-bg flex px-10 py-5 mx-15 my-10 gap-5 z-10 absolute left-48 top-0">
+      <div className="w-3/4 h-full form-bg flex px-10 py-5 mx-15 my-10 gap-5 z-10 absolute left-48 top-0 h-5/6">
         <form className="flex flex-col gap-5 rounded w-1/2">
           <div className="text-3xl">Contact Me</div>
+          <div className="text-xl gap-1 flex flex-col">
+            <div className="flex gap-5 justify-start">
+              <img
+                src="./images/bi_phone.png"
+                alt="contact_phone"
+                className="h-5 w-5"
+              />
+              <div className="text-sm"> +91 7001967224</div>
+            </div>
+            <div className="flex gap-5 justify-start">
+              <img
+                src="./images/mailVector.png"
+                alt="contact_mail"
+                className="h-3 w-4"
+              />
+              <div className="text-sm"> bsuvraneel@gmail.com</div>
+            </div>
+          </div>
           <input
             type="text"
             className="w-full px-4 rounded"
@@ -50,10 +69,10 @@ const Contact = () => {
             Submit
           </button>
         </form>
-      <div
-        className="calendly-inline-widget w-1/2 h-full opacity-90"
-        data-url="https://calendly.com/suvraneel/meet"
-      ></div>
+        <div
+          className="calendly-inline-widget w-1/2 h-full opacity-90"
+          data-url="https://calendly.com/suvraneel/meet"
+        ></div>
       </div>
 
       {/* <div
