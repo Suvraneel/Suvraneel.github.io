f018694a4c241749062ec4b3a48d57e1efee13bc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-08 20:59:11 +0530

    Added Work JSON

--- components/ChronoCard.js
+++ components/ChronoCard.js
@@ -1,26 +1,48 @@
 import React, { useState } from "react";
+import Image from "next/image";
 export default function ChronoCard({ curElem }) {
   const [isShown, setIsShown] = useState(false);
   return (
     <div className="flex md:contents">
-      <div className="col-start-1 col-end-4 md:mx-auto relative">
+      <div className="col-start-2 col-end-3 md:mx-auto relative">
         <div className="h-full w-6 flex items-center justify-center">
           <div className="h-full w-1 bg-white pointer-events-none" />
         </div>
         <div
-          className={"w-6 h-6 absolute top-1/2 -mt-3 border-4 rounded-full bg-slate-700 shadow text-center "+(isShown ? "animate-ping" : "")}
+          className={
+            "w-6 h-6 absolute top-1/2 -mt-3 border-4 rounded-full bg-slate-700 shadow text-center " +
+            (isShown ? "animate-ping" : "")
+          }
           onMouseEnter={() => setIsShown(true)}
           onMouseLeave={() => setIsShown(false)}
         />
       </div>
+      <div className="col-start-3 col-end-5 md:mx-auto flex justify-center items-center">
+        <Image
+          src={"/images/work-assets/" + curElem.image}
+          alt={curElem.company + "Logo"}
+          width={100}
+          height={100}
+        />
+      </div>
       <div
-        className={"border-4 text-white dark:text-white col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto w-full "+(isShown?"bg-cyan-900":"")}
+        className={
+          "border-4 text-white dark:text-white col-start-5 col-end-12 p-4 rounded-xl my-4 mr-auto w-full px-8 " +
+          (isShown ? "bg-cyan-900" : "")
+        }
         onMouseEnter={() => setIsShown(true)}
         onMouseLeave={() => setIsShown(false)}
       >
-        <h2 className="text-lg w-full">{curElem.role}</h2>
-        <p className="text-lg w-full">{curElem.company}</p>
-        <h3 className="font-bold text-xl mb-1">{curElem.duration}</h3>
+        <div className="text-lg w-full">{curElem.role}</div>
+        <div className="text-lg w-full">{curElem.company}</div>
+        <div className="font-bold text-md mb-1">{curElem.duration}</div>
+        <div className="text-sm w-full">
+          <ul className="list-disc">
+            {curElem.details.split("|").map((details) => (
+              <li key={details}>{details}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
