cf873fb06c24127cfb6761e845c103adf3030903
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-10-27 19:02:19 +0530

    codemod next-image-to-legacy-image : Port :two:

--- components/ChronoCard.js
+++ components/ChronoCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import useSound from "use-sound";
 export default function ChronoCard({ curElem }) {
   const [isShown, setIsShown] = useState(false);
