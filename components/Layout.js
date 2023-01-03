3ff866a29c8259390ab6d5e44e3878666a225445
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-01-03 23:40:55 +0530

    Added Page Hits & Console Ascii Art :art: :egg:

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import AsciiArt from "./AsciiArt";
 import Hamburger from "./Hamburger";
 import { Navbar } from "./Navbar";
 
@@ -6,7 +7,7 @@ export const siteTitle = "Next.js Portfolio Website";
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-transparent flex flex-row justify-start w-screen h-screen">
+    <div className="bg-transparent flex flex-row justify-start w-screen h-screen" id="visits">
       <div className="flex flex-col h-full w-full">
         {children}
         <Navbar />
