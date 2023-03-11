52db6a5ec488cc764184e33ebab14b3a036f3399
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-03-11 20:23:29 +0530

    migrated to typescript :statue_of_liberty:

--- stateLogger.ts
+++ stateLogger.ts
@@ -1,7 +1,7 @@
 // Tool for tracking and labeling animation/component state
 const log = console.log;
-export const framerLogger = (label) => log(`%c${label}: animation complete`, "color: red");
-export const stateLogger = (label, mounted) => {
+export const framerLogger = (label: string) => log(`%c${label}: animation complete`, "color: red");
+export const stateLogger = (label: string, mounted: boolean) => {
   mounted
     ? log(`%c${label}: mounted`, "color: green")
     : log(`%c${label}: unmounted`, "color: orange");
